41e628edbf1ee9dc416a0a415f30d309f1ae7702
mirumi <mail@mirumi.me>
2022-05-23 17:39:09 +0900

    Add files via upload

--- shared.ts
+++ shared.ts
@@ -1,62 +1,88 @@
-namespace ts.server {
-    export type ActionSet = "action::set";
-    export type ActionInvalidate = "action::invalidate";
-    export type ActionPackageInstalled = "action::packageInstalled";
-    export type EventTypesRegistry = "event::typesRegistry";
-    export type EventBeginInstallTypes = "event::beginInstallTypes";
-    export type EventEndInstallTypes = "event::endInstallTypes";
-    export type EventInitializationFailed = "event::initializationFailed";
-    /* @internal */
-    export const ActionSet: ActionSet = "action::set";
-    /* @internal */
-    export const ActionInvalidate: ActionInvalidate = "action::invalidate";
-    /* @internal */
-    export const ActionPackageInstalled: ActionPackageInstalled = "action::packageInstalled";
-    /* @internal */
-    export const EventTypesRegistry: EventTypesRegistry = "event::typesRegistry";
-    /* @internal */
-    export const EventBeginInstallTypes: EventBeginInstallTypes = "event::beginInstallTypes";
-    /* @internal */
-    export const EventEndInstallTypes: EventEndInstallTypes = "event::endInstallTypes";
-    /* @internal */
-    export const EventInitializationFailed: EventInitializationFailed = "event::initializationFailed";
-
-    /* @internal */
-    export namespace Arguments {
-        export const GlobalCacheLocation = "--globalTypingsCacheLocation";
-        export const LogFile = "--logFile";
-        export const EnableTelemetry = "--enableTelemetry";
-        export const TypingSafeListLocation = "--typingSafeListLocation";
-        export const TypesMapLocation = "--typesMapLocation";
-        /**
-         * This argument specifies the location of the NPM executable.
-         * typingsInstaller will run the command with `${npmLocation} install ...`.
-         */
-        export const NpmLocation = "--npmLocation";
-        /**
-         * Flag indicating that the typings installer should try to validate the default npm location.
-         * If the default npm is not found when this flag is enabled, fallback to `npm install`
-         */
-        export const ValidateDefaultNpmLocation = "--validateDefaultNpmLocation";
+namespace Harness.Parallel {
+    export interface RunnerTask {
+        runner: TestRunnerKind;
+        file: string;
+        size: number;
     }
 
-    /* @internal */
-    export function hasArgument(argumentName: string) {
-        return sys.args.indexOf(argumentName) >= 0;
+    export interface UnitTestTask {
+        runner: "unittest";
+        file: string;
+        size: number;
     }
 
-    /* @internal */
-    export function findArgument(argumentName: string): string | undefined {
-        const index = sys.args.indexOf(argumentName);
-        return index >= 0 && index < sys.args.length - 1
-            ? sys.args[index + 1]
-            : undefined;
+    export type Task = RunnerTask | UnitTestTask;
+
+    export interface TestInfo {
+        name: string[];
     }
 
-    /* @internal */
-    export function nowString() {
-        // E.g. "12:34:56.789"
-        const d = new Date();
-        return `${padLeft(d.getHours().toString(), 2, "0")}:${padLeft(d.getMinutes().toString(), 2, "0")}:${padLeft(d.getSeconds().toString(), 2, "0")}.${padLeft(d.getMilliseconds().toString(), 3, "0")}`;
+    export interface ErrorInfo {
+        name: string[];
+        error: string;
+        stack: string;
+    }
+
+    export interface TaskTimeout {
+        duration: number | "reset";
+    }
+
+    export interface TaskResult {
+        passing: number;
+        errors: ErrorInfo[];
+        passes: TestInfo[];
+        duration: number;
+        task: Task;
+    }
+
+    export interface ParallelTestMessage {
+        type: "test";
+        payload: Task;
+    }
+
+    export interface ParallelBatchMessage {
+        type: "batch";
+        payload: Task[];
+    }
+
+    export interface ParallelCloseMessage {
+        type: "close";
+    }
+
+    export type ParallelHostMessage = ParallelTestMessage | ParallelCloseMessage | ParallelBatchMessage;
+
+    export interface ParallelErrorMessage {
+        type: "error";
+        payload: { error: string, stack: string, name?: string[] };
+    }
+
+    export interface ParallelResultMessage {
+        type: "result";
+        payload: TaskResult;
+    }
+
+    export interface ParallelBatchProgressMessage {
+        type: "progress";
+        payload: TaskResult;
+    }
+
+    export interface ParallelTimeoutChangeMessage {
+        type: "timeout";
+        payload: TaskTimeout;
+    }
+
+    export type ParallelClientMessage = ParallelErrorMessage | ParallelResultMessage | ParallelBatchProgressMessage | ParallelTimeoutChangeMessage;
+
+    export function shimNoopTestInterface(global: Mocha.MochaGlobals) {
+        global.before = ts.noop;
+        global.after = ts.noop;
+        global.beforeEach = ts.noop;
+        global.afterEach = ts.noop;
+        global.describe = global.context = ((_: any, __: any) => { /*empty*/ }) as Mocha.SuiteFunction;
+        global.describe.skip = global.xdescribe = global.xcontext = ts.noop as Mocha.PendingSuiteFunction;
+        global.describe.only = ts.noop as Mocha.ExclusiveSuiteFunction;
+        global.it = global.specify = ((_: any, __: any) => { /*empty*/ }) as Mocha.TestFunction;
+        global.it.skip = global.xit = global.xspecify = ts.noop as Mocha.PendingTestFunction;
+        global.it.only = ts.noop as Mocha.ExclusiveTestFunction;
     }
 }
